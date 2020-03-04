7eb9a2f122d4d5b18c4fbf67ff60acc6c779c9c6
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-03-04 23:03:43 +0000

    added students and teachers .js

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+const limit = process.argv[3];
+// const values = [`%${cohortName}%`, limit];
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [`%${cohortName}%`, limit])
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));;
